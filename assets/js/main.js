3c355cca5626bf23c7c946016092845bd9c76f47
Michael Andrew <michael.andrew@gladeye.com>
2017-07-23 16:00:46 +1200

    Added support for picking up gameboy using cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,10 @@
 AFRAME.registerSystem('main', {
     schema: {},  // System schema. Parses into `this.data`.
+
+    followingEntity: '', //ID of entity currently following the cursor
+    entityOriginalAttrs: {}, //attributes of original entities before they are set to follow cursor
+    targettedEntity: '',
+
     init: function () {
         // Called on scene initialization.
 
@@ -9,7 +14,9 @@ AFRAME.registerSystem('main', {
 
         var scene = document.querySelector('a-scene');
         var mainCanvas = document.getElementById("mainCanvas");
-        var gameboy = document.getElementById("collision-gameboy");
+        var cursorEl = document.getElementById("my-cursor");
+        this.gameboy = document.getElementById("gameboy");
+        this.gameboyCollider = document.querySelector("#gameboy .collider");
 
         scene.addEventListener('loaded',function(){
 
@@ -39,18 +46,103 @@ AFRAME.registerSystem('main', {
             };
             client.send();
 
+            var colliderEls = AFRAME.utils.styleParser.parse(cursorEl.getAttribute('sphere-collider')).objects.split(',');
 
+            //console.log('Els: ',colliderEls)
+
+            for(var i = 0; i < colliderEls.length; i++){
+                var el = document.querySelector(colliderEls[i]);
+
+                el.addEventListener('hit',function(e){
+
+                    //console.log('hit', e.target);
+
+                    var targetEl = (e.target.parentNode !== self.el.sceneEl) ? e.target.parentNode : e.target;
+
+                    //console.log(targetEl);
+
+                    //if(targetEl.object3D){
+                        self.targettedEntity = targetEl.getAttribute('id');
+                    //}
+
+                    //console.log('Targetted: ',self.targettedEntity);
+                });
+            }
 
 
         }.bind(this));
 
-        gameboy.addEventListener('hit',function(e){
-            console.log('Hit gameboy');
-        })
+        window.addEventListener('mouseup',function(e){
+
+            //console.log('clicked: ',this.targettedEntity);
+            if(this.targettedEntity !== ''){
+                var targetEl = document.querySelector('#'+this.targettedEntity);
+                var colliderEl = document.querySelector('#'+this.targettedEntity+' .collider');
+                switch (this.targettedEntity){
+                    case 'gameboy':
+                        this.toggleEntityFollowCursor(this.targettedEntity,targetEl,colliderEl);
+                        break;
+                    case 'collision-table':
+                    case 'collision-floor':
+
+                        this.entityUnfollowCursor('gameboy',self.gameboy,self.gameboyCollider);
+                        break;
+                    default:
+                        //do nothing
+                        break;
+                }
+            }
+        }.bind(this));
+
+    },
+
+    toggleEntityFollowCursor: function(id,entity,colliderEl){
+        //console.log('toggling');
+        if(id === this.followingEntity){
+            this.entityUnfollowCursor(id,entity,colliderEl);
+        }else{
+            this.entityFollowCursor(id,entity,colliderEl);
+        }
+    },
+
+    entityFollowCursor: function(id,entity,colliderEl){
+        //if(this.followingEntity !== id){
+            //console.log(id+' is now following cursor');
+            this.followingEntity = id;
+            this.entityOriginalAttrs[id] = {
+                'grabbable': entity.getAttribute('grabbable'),
+                'dynamic-body': entity.getAttribute('dynamic-body')
+            };
+            entity.removeAttribute('grabbable');
+            entity.removeAttribute('dynamic-body');
+            entity.setAttribute('look-at','#camera');
+            entity.setAttribute('follow','target','#my-cursor');
+            colliderEl.classList.add('ignore-ray');
+            //this.cursor.removeAttribute('sphere-collider');
+        //}
+
+
+        //console.log(this.followingEntity);
+    },
+
+    entityUnfollowCursor: function(id,entity,colliderEl){
+
+        //console.log(id+' is now unfollowing cursor');
+        this.followingEntity = '';
+        this.targettedEntity = '';
+        if(this.entityOriginalAttrs[id]){
+            entity.setAttribute('grabbable',this.entityOriginalAttrs[id]['grabbable']);
+            entity.setAttribute('dynamic-body',this.entityOriginalAttrs[id]['dynamic-body']);
+        }
+
+        entity.removeAttribute('follow');
+        entity.removeAttribute('look-at');
+        colliderEl.classList.remove('ignore-ray');
     },
 
     tick: function (t, dt) {
         //console.log(self.camera.getAttribute('position'));
+
 
     }
     // Other handlers and methods.
