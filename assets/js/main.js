4af03c9faec479fd5c608481bb7fe73016232c3c
Michael Andrew <michael.andrew@gladeye.com>
2017-08-04 21:06:33 +1200

    Now exclusively using super-hands
    super-hands component now fully takes care of handling drag / grab states for all draggable items in scene, greatly reducing the amount of custom code we need to implement this functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,19 +11,12 @@ AFRAME.registerSystem('main', {
 
         //do stuff here after scene initializes
 
-        var self = this;
-
         var scene = document.querySelector('a-scene');
         var mainCanvas = document.getElementById("mainCanvas");
         this.cursorEl = document.getElementById("my-cursor");
         this.gameboy = document.getElementById("gameboy");
-        this.tvScreen = document.getElementById("tv-screen");
-        this.gameboyCollider = document.querySelector("#gameboy .collider");
-
 
         scene.addEventListener('loaded',function(){
-
-
 
             var client = new XMLHttpRequest();
             //client.open('GET', 'assets/roms/pd/geometrix/geometrix.gbc.base64.txt');
@@ -45,58 +38,18 @@ AFRAME.registerSystem('main', {
                     }
                 }
 
-                //alert(client.responseText);
-                //console.log('Loaded: ',client.responseText);
-
             };
             client.send();
 
-            var colliderEls = AFRAME.utils.styleParser.parse(self.cursorEl.getAttribute('sphere-collider')).objects.split(',');
+            var superHands = this.cursorEl.components['super-hands'];
 
-            //console.log('Els: ',colliderEls)
+            window.addEventListener('mousedown',function(e){
+                superHands.onGrabStartButton();
+            });
 
-            for(var i = 0; i < colliderEls.length; i++){
-                var el = document.querySelector(colliderEls[i]);
-
-                //console.log('Added collider to: ',el.getAttribute('id'));
-
-                el.addEventListener('hit',function(e){
-
-                    //console.log('hit', e.target);
-
-                    var targetEl = (e.target.parentNode !== self.el.sceneEl) ? e.target.parentNode : e.target;
-
-
-                    //console.log('targetEl: ',targetEl);
-
-                    //if(targetEl.object3D){
-                    //if(!self.followingEntity){
-                        self.targettedEntity = targetEl.getAttribute('id');
-                    //}
-
-
-                    /*if(self.targettedEntity === 'gameboy'){
-                        self.cursorEl.setAttribute('material','opacity',0.5);
-                    }else{
-                        self.cursorEl.setAttribute('material','opacity',1);
-                    }*/
-                    //}
-
-                    //console.log('Targetted: ',self.targettedEntity);
-                });
-            }
-
-            //this.entityUnfollowCursor('gameboy',this.gameboy,this.gameboyCollider);
-
-            /*setTimeout(function(){
-                this.giphyTVEl = document.querySelector('#asdf-draggable-gif');
-
-                setInterval(function(){
-                    console.log('src: ',this.giphyTVEl.getAttribute('src'));
-                    this.tvScreen.setAttribute('material','src:url('+this.giphyTVEl.getAttribute('src')+')');
-                }.bind(this),5000); //switch TV display every 5 seconds
-            }.bind(this),5000);*/
-
+            window.addEventListener('mouseup',function(e){
+                superHands.onGrabEndButton();
+            });
 
         }.bind(this));
 
@@ -105,120 +58,10 @@ AFRAME.registerSystem('main', {
             console.log('Gameboy respawned');
         }.bind(this));
 
-        window.addEventListener('mouseup',function(e){
-
-            if(this.firstClick){
-                this.firstClick = false;
-                return;
-            }
-
-            //console.log('clicked: ',this.targettedEntity);
-            if(this.targettedEntity !== ''){
-                var targetEl = document.querySelector('#'+this.targettedEntity);
-                var colliderEl = targetEl.querySelector('.collider');
-                switch (this.targettedEntity){
-                    case 'gameboy':
-                        this.toggleEntityFollowCursor(this.targettedEntity,targetEl,colliderEl);
-                        break;
-                    case 'glass':
-                    case 'picture':
-                    case 'ball':
-                        this.toggleEntityFollowCursor(this.targettedEntity,targetEl,targetEl);
-                        break;
-                    case 'collision-table':
-                    case 'collision-floor':
-
-                        //this.entityUnfollowCursor('gameboy',self.gameboy,self.gameboyCollider);
-                        break;
-                    default:
-                        //do nothing
-                        break;
-                }
-            }
-        }.bind(this));
-
-
-
-    },
-
-    toggleEntityFollowCursor: function(id,entity,colliderEl){
-        //console.log('toggling');
-        if(id === this.followingEntity){
-            this.entityUnfollowCursor(id,entity,colliderEl);
-        }else{
-            this.entityFollowCursor(id,entity,colliderEl);
-        }
-    },
-
-    entityFollowCursor: function(id,entity,colliderEl){
-        //if(this.followingEntity !== id){
-            //console.log(id+' is now following cursor');
-
-            this.entityOriginalAttrs[id] = {
-                'grabbable': entity.getAttribute('grabbable'),
-                'dynamic-body': entity.getAttribute('dynamic-body')
-            };
-
-            switch(id){
-                case 'gameboy':
-                    this.cursorEl.setAttribute('material','opacity',0);
-                    break;
-                case 'ball':
-                    this.cursorEl.setAttribute('material','opacity',0);
-                    this.entityOriginalAttrs[id].mirror = entity.getAttribute('mirror');
-                    //entity.setAttribute('mirror','repeat',false);
-                    //entity.removeAttribute('mirror');
-                    break;
-            }
-
-            this.followingEntity = id;
-
-            entity.removeAttribute('grabbable');
-            entity.removeAttribute('dynamic-body');
-            entity.setAttribute('look-at','#camera');
-            entity.setAttribute('follow','target','#my-cursor');
-
-            colliderEl.classList.add('ignore-ray');
-
-            //this.cursor.setAttribute('sphere-collider','objects','#collision-gameboy,#glass');
-
-
-        console.log('now ignoring: ',colliderEl);
-
-            //this.cursor.removeAttribute('sphere-collider');
-        //}
-
-
-        //console.log(this.followingEntity);
-    },
-
-    entityUnfollowCursor: function(id,entity,colliderEl){
-
-        //console.log(id+' is now unfollowing cursor');
-        this.followingEntity = '';
-        this.targettedEntity = '';
-        if(this.entityOriginalAttrs[id]){
-            entity.setAttribute('grabbable',this.entityOriginalAttrs[id]['grabbable']);
-            entity.setAttribute('dynamic-body',this.entityOriginalAttrs[id]['dynamic-body']);
-
-            if(id === 'ball'){
-                //entity.setAttribute('mirror',this.entityOriginalAttrs[id]['mirror']);
-            }
-        }
-
-        entity.removeAttribute('follow');
-        entity.removeAttribute('look-at');
-        this.cursorEl.setAttribute('material','opacity',0.5);
-        colliderEl.classList.remove('ignore-ray');
 
     },
 
     tick: function (t, dt) {
-        //console.log(self.camera.getAttribute('position'));
-
-
-
-
 
     }
     // Other handlers and methods.
