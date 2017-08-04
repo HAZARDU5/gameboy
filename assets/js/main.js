13b23220a39e806bfef409701b00af11a58d3ed7
Michael Andrew <michael.andrew@gladeye.com>
2017-08-04 12:58:38 +1200

    Added new public domain game “tuff”

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,7 +26,9 @@ AFRAME.registerSystem('main', {
 
 
             var client = new XMLHttpRequest();
-            client.open('GET', 'assets/roms/pd/geometrix/geometrix.gbc.base64.txt');
+            //client.open('GET', 'assets/roms/pd/geometrix/geometrix.gbc.base64.txt');
+            client.open('GET', 'assets/roms/pd/tuff/tuff.gb.base64.txt');
+
             //client.open('GET', 'assets/roms/commercial/super-mario-land/super-mario-land.gb.base64.txt');
             client.onreadystatechange = function() {
 
